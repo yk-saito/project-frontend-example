4ae49d55078e68bec166284a59d4d0dfd6113505
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-12-17 16:47:15 +0900

    fix: type number is not assignable to type string

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ const Page = () => {
   if (yearElement.current) {
     const date = new Date();
     const year = date.getFullYear();
-    yearElement.current.textContent = year;
+    yearElement.current.textContent = String(year);
   }
 
   const layoutStyle = (tabValue: TabState) => css`
